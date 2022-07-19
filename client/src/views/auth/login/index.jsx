48f75f23b92e4f1c682481a8bf35d7ead50cfb6d
foussedeve <foussedev@gmail.com>
2022-07-19 09:40:20 +0200

    init frontend code base

---
+++ client/src/views/auth/login/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import session from "../../../session";
+import fakeApi from "../../../services/fakeApi";
+
+const Login=()=>{
+
+
+    return(
+
+        <>
+        Login
+        </>
+    )
+}
+
+export default Login;
